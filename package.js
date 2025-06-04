3566d47e8695a219afd1d3d6ff712be186bd5969
Stasonnnn <staszelenskiy@gmail.com>
2025-06-04 18:40:44 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "telegram-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "@a2seven/yoo-checkout": "^1.1.4",
+    "axios": "^1.9.0",
+    "body-parser": "^2.2.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.5.0",
+    "express": "^4.21.2",
+    "node-fetch": "^3.3.2",
+    "node-telegram-bot-api": "^0.61.0",
+    "uuid": "^11.1.0",
+    "yookassa": "^0.1.1"
+  }
+}
